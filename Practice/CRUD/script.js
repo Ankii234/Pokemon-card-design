028cc58c7bd510c83914ca3429f43d4dc390288d
Ankit SIngh <maddens3702h@gmail.com>
2026-09-08 13:14:54 +0530

    crud10

---
+++ Practice/CRUD/script.js
@@ -0,0 +1,7 @@
+const createBtn = document.querySelector("#create");
+const formDiv = document.querySelector(".form");
+
+createBtn.addEventListener("click", () => {
+    formDiv.style.display = "flex";
+
+});
